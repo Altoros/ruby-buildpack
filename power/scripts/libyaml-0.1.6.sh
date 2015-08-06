644e7328f1823127b8ba8c3178ed8a14d00b61ac
A.S. Lomoff <lomov.as@gmail.com>
2015-08-06 17:15:25 +0300

    fixes

--- power/scripts/libyaml-0.1.6.sh
+++ power/scripts/libyaml-0.1.6.sh
@@ -3,8 +3,8 @@ set -xe
 
 tar -xzvf yaml-0.1.6.tar.gz
 cd yaml-0.1.6
-curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.guess" > ./config
-curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.sub" > ./config
+curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.guess" > ./config/config.guess
+curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.sub" > ./config/config.sub
 CFLAGS='-fPIC' ./configure --prefix=${install_target} --disable-shared
 make
 make install
--- power/scripts/ruby-2.1.5.sh
+++ power/scripts/ruby-2.1.5.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-tar xzf ruby-2.1.5.tar.gz
+tar -xzvf ruby-2.1.5.tar.gz
 cd ruby-2.1.5
-curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.guess" > ./tool
-curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.sub" > ./tool
+curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.guess" > ./tool/config.guess
+curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.sub" > ./tool/config.sub
 LDFLAGS="-Wl,-rpath -Wl,${install_target}" CFLAGS='-fPIC' ./configure --prefix=${install_target} --disable-install-doc --with-opt-dir=${install_target} --without-gmp
 make
 make install
