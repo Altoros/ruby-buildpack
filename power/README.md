0c8c9b00a9e9003ea6756e182cf12292c40bdc41
A.S. Lomoff <lomov.as@gmail.com>
2015-08-06 20:50:59 +0300

    first try to use normal manifest

--- power/README.md
+++ power/README.md
@@ -1,4 +1,9 @@
 
 ```
+bundle install
+cp secret.yml{.example,}
+vi secret.yml
+
 ./bin/build-all manifest.yml
+bundle exec ./bin/upload-all manifest.yml secret.yml
 ```
--- power/scripts/libyaml-0.1.6.sh
+++ power/scripts/libyaml-0.1.6.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xe
 
-tar -xzvf yaml-0.1.6.tar.gz
+tar -xzvf libyaml-0.1.6.tar.gz
 cd yaml-0.1.6
 curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.guess" > ./config/config.guess
 curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.sub" > ./config/config.sub
