ad293e2d7ecc9102189a8a4f076945064b1fa23e
A.S. Lomoff <lomov.as@gmail.com>
2015-08-11 14:23:58 +0300

    add ruby assets to run it without errors

--- power/README.md
+++ power/README.md
@@ -7,3 +7,9 @@ vi secret.yml
 ./bin/build-all manifest.yml
 bundle exec ./bin/upload-all manifest.yml secret.yml
 ```
+
+```
+vi /home/ubuntu/.rvm/gems/ruby-2.2.2/bundler/gems/buildpack-packager-58ccd566aee3/lib/buildpack/packager/manifest_schema.yml
+CF_STACK=trustyppc64le BUNDLE_GEMFILE=cf.Gemfile bundle exec buildpack-packager cached
+
+```
--- power/scripts/ruby-2.1.5.sh
+++ power/scripts/ruby-2.1.5.sh
@@ -9,3 +9,10 @@ curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=co
 LDFLAGS="-Wl,-rpath -Wl,${install_target}" CFLAGS='-fPIC' ./configure --prefix=${install_target} --disable-install-doc --with-opt-dir=${install_target} --without-gmp
 make
 make install
+
+
+tar zxvf rubygems-2.1.11.tgz
+cd rubygems-2.1.11
+${install_target}/bin/ruby setup.rb
+
+${install_target}/bin/gem install ruby-2.1.6/bundler-1.9.4.gem --no-ri --no-rdoc
