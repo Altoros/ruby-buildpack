a0327930e112d17a31e55609a39acb5d9721bdbe
A.S. Lomoff <lomov.as@gmail.com>
2015-08-11 17:22:27 +0300

    add ruby configure flag

--- power/scripts/ruby-2.1.5.sh
+++ power/scripts/ruby-2.1.5.sh
@@ -6,7 +6,7 @@ tar -xzvf ruby-2.1.5.tar.gz
 pushd ruby-2.1.5
   curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.guess" > ./tool/config.guess
   curl "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;hb=HEAD;f=config.sub" > ./tool/config.sub
-  LDFLAGS="-Wl,-rpath -Wl,${install_target}" CFLAGS='-fPIC' ./configure --prefix=${install_target} --disable-install-doc --with-opt-dir=${install_target} --without-gmp
+  LDFLAGS="-Wl,-rpath -Wl,${install_target}" CFLAGS='-fPIC' ./configure --prefix=${install_target} --disable-install-doc --with-opt-dir=${install_target} --without-gmp --enable-load-relative
   make
   make install
 popd
